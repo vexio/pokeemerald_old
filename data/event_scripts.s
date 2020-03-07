e511c2df275552ff92f5fe8de9bb242a20ccdd12
Bryce <brycewhoffhous@gmail.com>
2020-03-06 18:21:06 -0800

    Re-mapping Phase 1
    Remapped:
    Canvas Town
    Route 1 (Canvas Path)
    Alnerwick City
    Route 2 (Alnerwick Hike)
    Gatehouse Tiles

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1059,3 +1059,7 @@ Common_EventScript_LegendaryFlewAway:: @ 8273776
 	.include "data/text/frontier_brain.inc"
 	.include "data/text/save.inc"
 	.include "data/text/birch_speech.inc"
+
+	.include "data/maps/Route103_/scripts.inc"
+
+	.include "data/maps/Route103_Gatehouse/scripts.inc"
